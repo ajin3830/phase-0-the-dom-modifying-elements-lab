abf8e7973fedb1591322f465b21b442f92d8149d
ajin3830 <ajin3830@gmail.com>
2023-01-12 10:44:50 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent ="<h1>AJ is the champion</h1>";
+
+document.querySelector('#something').appendChild(newHeader)
